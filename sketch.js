e40ac435e75c154149ec24fa6a637b58952fdccb
Raghavkhandelwal7 <71634967+Raghavkhandelwal7@users.noreply.github.com>
2020-09-30 08:33:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
